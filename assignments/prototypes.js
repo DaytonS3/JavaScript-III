2c9f5124fd91fbd5ba507a1c30336b657ca6846d
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-02-28 15:15:31 -0500

    Added classes

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,71 +1,55 @@
-/*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
-
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
-
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
-  Each constructor function has unique properties and methods that are defined in their block comments below:
-*/
 
 
-//----------------------------------------------------------------------
-/*
-  === GameObject ===
-  * createdAt
-  * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-*/
-function GameObjects(att) {
+
+
+
+class GameObjects {
+   constructor(att) {
   this.createdAt = att.createdAt;
   this.name = att.name;
   this.dimensions = att.dimensions;
 }
 
-GameObjects.prototype.destroy = function () {
+destroy () {
   return `${this.name} was removed from the game.`;
-};
+ };
+}; //GameObjects
 
-//-------------------------------------------------------------------
-/*
-  === CharacterStats ===
-  * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
-function CharacterStats (CharAtt) {
+
+class CharacterStats extends GameObjects{
+    constructor(CharAtt) {
+      super(CharAtt)
   this.healthPoints = CharAtt.healthPoints;
-  GameObjects.call(this, CharAtt);
-}
-CharacterStats.prototype = Object.create(GameObjects.prototype);
-CharacterStats.prototype.takeDamage = function () {
-  return `${this.name} took damage.`
 };
 
-//----------------------------------------------------------------------
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
- function Humanoid (HumAtt) {
+takeDamage() {
+  return `${this.name} took damage.`
+ };
+};//CharacterStats
+
+
+class Humanoid extends CharacterStats {
+    constructor(HumAtt) {
+      super(HumAtt)
   this.team = HumAtt.team;
   this.weapons = HumAtt.weapons;
   this.language = HumAtt.language;
-  CharacterStats.call(this, HumAtt);
 };
 
-Humanoid.prototype = Object.create(CharacterStats.prototype);
-Humanoid.prototype.greet = function () {
-  return `${this.name} offers a greeting in ${this.language}.`
-}
 
-//-------------------------------------------------------------------
+greet() {
+  return `${this.name} offers a greeting in ${this.language}.`
+ };
+};//Humanoid
+
+
+
+
+
+
+
+
+
 
 function Villain (VilAtt) {
   Humanoid.call(this, VilAtt);
@@ -84,16 +68,10 @@ Villain.prototype.bat = function (opponent) {
 
 
 
-
-
-
-
-//---------------------------------------------------------------------
-
 function Hero (HeroAtt) {
   Humanoid.call(this, HeroAtt);
 }
-
+ 
 Hero.prototype = Object.create(Humanoid.prototype);
 Hero.prototype.force = function (opp) {
   if (opp.healthPoints <= 0) {
@@ -106,21 +84,6 @@ Hero.prototype.force = function (opp) {
 
 
 
-
-//--------------------------------------------------------------------
-
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
-
-/*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
-
-// Test you work by un-commenting these 3 objects and the list of console logs below:
 
 
   const mage = new Humanoid({
